51cf13607c90ab60a10b99033217cbfd10139d16
Tomoyuki Sakurai <y@trombik.org>
2019-12-27 15:37:18 +0900

    bugfix: remove `if else` for old version of unbound
    even Ubuntu 16.04 has recent version of unbound.
    
    while here:
    
    * remove empty directory for test_plugins
    * update FreeBSD version for x509 test, and update the test accordingly

--- tests/serverspec/x509_spec.rb
+++ tests/serverspec/x509_spec.rb
@@ -6,7 +6,7 @@ describe port(tls_port) do
 end
 
 describe command("echo | openssl s_client -connect 127.0.0.1:#{tls_port} -showcerts") do
-  its(:stdout) { should match(/#{Regexp.escape("issuer=/C=AU/ST=Some-State/O=Internet Widgits Pty Ltd/CN=foo.example.org")}/) }
+  its(:stdout) { should match(/#{Regexp.escape("issuer=C = AU, ST = Some-State, O = Internet Widgits Pty Ltd, CN = foo.example.org")}/) }
   its(:stderr) { should match(/verify error:num=18:self signed certificate/) }
   its(:exit_status) { should eq 0 }
 end
